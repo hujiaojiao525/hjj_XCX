53567a27a2fcf6e0cd04ab14e3b8da579b7a3417
胡娇娇 <hujiaojiao@cheyipai.com>
2018-12-11 19:23:13 +0800

    添加地图组件

--- src/pages/indexMap/index.vue
+++ src/pages/indexMap/index.vue
@@ -1,48 +1,77 @@
 <template>
     <div class="index-map">
-        <section class="map-top">
+    	<!-- 顶部 -->
+        <div class="map-top">
         	<div class="search-box">
         		<image class="search-img" src="../../static/image/search.png"></image>
         		<input class="search-input" type="text" placeholder="输入目的地/电站名" readonly=true>
         	</div>
         	<image class="listImg-img" src="../../static/image/listImg.png"></image>
-        </section>
-        <section class="map-box">
-        	<div class="left-img">
-        		<!-- 编辑 -->
-        		<div class="img-box" @click="editFun">
-        			<image src="../../static/image/editImg.png"></image>
-        		</div>
-        		<!-- 筛选 -->
-        		<div class="img-box">
-        			<image src="../../static/image/screen.png"></image>
-        		</div>
-        		<!-- 定位 -->
-        		<div class="img-box">
-        			<image src="../../static/image/posImg.png"></image>
-        		</div>
-        	</div>
-        	<!-- 扫码 -->
-        	<div class="sweep-box">
-        		<image src="../../static/image/sweepCode.png"></image>
-        		<span>扫码充电</span>
-        	</div>
-        	<!-- 个人中心 -->
-        	<div class="right-img">
-        		<div class="img-box" @click="goToMy">
-        			<image src="../../static/image/myImg.png"></image>
-        		</div>
-        	</div>
-        </section>
-        <!-- 编辑弹框 -->
-        <section class="layer" v-show="editPop">
-        	<div class="edit-pop" :class="{editLayer: editPop}">
-	        	<image @click="closePop" class="close-btn" src="../../static/image/close.png"></image>
-	        	<p class="edit-title">输入终端编号充电</p>
-	        	<p class="edit-mes">终端编号位于充电终端二维码标签处，输入终端编号开启充电</p>
+        </div>
+        <div class="top-layer" v-show="editPop"></div>
+    	<map 
+            id="map" 
+            :markers="markers" 
+            scale="14"
+            subkey="2AJBZ-GDVWW-CVYRV-O5UML-R66UK-GEFRL"
+            :style="{height: WinHeight+'px'}"
+            :latitude="latitude"
+            :longitude="longitude"
+            @callouttap="goToClass" 
+            @end="regionchange"
+            @begin="regionchange"
+            @regionchange="regionchange"
+            show-location >
+           <cover-view class="map-box">
+        		<cover-view class="left-img">
+	        		<!-- 编辑 -->
+	        		<cover-view class="img-box" @tap="editFun">
+	        			<cover-image class="img" src="../../static/image/editImg.png"/>
+	        		</cover-view>
+	        		<!-- 筛选 -->
+	        		<cover-view class="img-box">
+	        			<cover-image class="img" src="../../static/image/screen.png"/>
+	        		</cover-view>
+	        		<!-- 定位 -->
+	        		<cover-view class="img-box">
+	        			<cover-image class="img" src="../../static/image/posImg.png"/>
+	        		</cover-view>
+		        	</cover-view>
+		        	<!-- 扫码 -->
+		        	<cover-view class="sweep-box">
+		        		<cover-image class="img" src="../../static/image/sweepCode.png"/>
+		        		<cover-view class="text">扫码充电</cover-view>
+		        	</cover-view>
+		        	<!-- 个人中心 -->
+		        	<cover-view class="right-img">
+	        		<cover-view class="img-box" @click="goToMy">
+	        			<cover-image class="img" src="../../static/image/myImg.png"/>
+	        		</cover-view>
+	        	</cover-view>
+	        </cover-view>
+	        <cover-view class="layer" v-show="editPop"></cover-view>
+        </map>
+        
+        <!-- 底部 -->
+
+        
+        <!-- 弹层 -->
+        <!-- <cover-view class="layer" v-show="editPop">
+        	<cover-view class="edit-pop" :class="{editLayer: editPop}">
+	        	<cover-image @click="closePop" class="close-btn" src="../../static/image/close.png"/>
+	        	<cover-view class="edit-title">输入终端编号充电</cover-view>
+	        	<cover-view class="edit-mes">终端编号位于充电终端二维码标签处，输入终端编号开启充电</cover-view>
 	        	<input type="text" class="edit-input" placeholder="清输入终端编号开启充电">
-	        </div>
-        </section>
+	        </cover-view>
+        </cover-view> -->
+        
+        <div class="edit-pop"  v-show="editPop" :class="{editLayer: editPop}">
+        	<image @click="closePop" class="close-btn" src="../../static/image/close.png"></image>
+        	<div class="edit-title">输入终端编号充电</div>
+        	<div class="edit-mes">终端编号位于充电终端二维码标签处，输入终端编号开启充电</div>
+        	<input type="text" :focus="focus" class="edit-input" placeholder="清输入终端编号开启充电">
+        </div>
+
     </div>
 </template>
 
@@ -52,26 +81,69 @@
         data() {
             return{
                 editPop: false,
+                focus: false,
+                WinHeight: '',
+                markers: [{ // 标记点用于在地图上显示标记的位置
+					id: 0,
+					latitude: 23.099994,
+					longitude: 113.324520,
+					name: 'T.I.T 创意园'
+			    }],
+			    latitude: '23.099994', // 中心纬度
+			    longitude: '113.324520' , // 中心经度	
+			    bottomHeight: '',
             }
         },
-        onshow(){
-
+        mounted(){
+        	const self = this;
+        	var query = wx.createSelectorQuery()
+            query.select('.map-top').boundingClientRect()
+            query.exec(function (hei) {
+                wx.getSystemInfo({ //是否为iphoneX
+	                success: function(res) {
+	                	console.log(hei[0].height)
+	                    self.WinHeight = res.windowHeight - hei[0].height
+	                }
+	            })
+            })
+            
+        	
         },
+        scroll(e) {
+            	console.log(e)
+            },
         methods: {
             goToMy() {
                 wx.navigateTo({
                     url: '/pages/my/main'
                 })
             },
+
             // 编辑
             editFun() {
             	const self = this;
             	self.editPop = true;
+            	setTimeout(function() {
+            		var query = wx.createSelectorQuery()
+	            	query.select('.edit-pop').boundingClientRect()
+		            query.exec(function (hei) {
+		                console.log(hei[0].height)
+		                self.bottomHeight = hei[0].height
+		                self.WinHeight = self.WinHeight -  hei[0].height
+		                
+		            })
+            	}, 50)
+            	setTimeout(function() {
+            		self.focus = true;
+            	}, 60)
+            	
             },
             // 关闭弹窗
             closePop() {
 				const self = this;
             	self.editPop = false;
+            	self.focus = false;
+            	self.WinHeight = self.WinHeight + self.bottomHeight
         	},
         },
     };
@@ -88,10 +160,15 @@
         from {bottom:-279rpx;}
         to {bottom:0px;}
     }
+    #map{
+    	width:100%;
+    	position: relative;
+	}
     .map-top{
     	padding: 15rpx;
     	background: #0086b3;
     	box-sizing: border-box;
+    	width: 100%;
     	position: relative;
     }
     .search-box{
@@ -103,6 +180,15 @@
     	box-sizing: border-box;
     	position: relative;
     }
+    .top-layer{
+    	background: rgba(0,0,0,.6);
+    	position: fixed;
+    	width: 100%;
+    	height: 100rpx;
+    	left:0;
+    	top:0;
+    	z-index:5;
+	}
     .search-img{
     	width: 30rpx;
     	height: 30rpx;
@@ -126,20 +212,30 @@
     	margin-top: -25rpx;
 
     }
+    .controls{
+    	position: fixed;
+    	left:0;
+    	bottom:200rpx;
+    }
     .map-box{
-    	position: relative;
+    	position: absolute;
+    	width: 100%;
+    	height:450rpx;
+    	left:0;
+    	bottom:90rpx;
+    	z-index:0;
     }
     .left-img{
-    	position: fixed;
+    	position: absolute;
     	width: 100rpx;
-    	left:80rpx;
-    	bottom:100rpx;
+    	left:60rpx;
+    	bottom:10rpx;
     }
     .right-img{
-    	position: fixed;
+    	position: absolute;
     	width: 100rpx;
-    	right:80rpx;
-    	bottom:100rpx;
+    	right:60rpx;
+    	bottom:10rpx;
     }
     .img-box{
     	width: 72rpx;
@@ -150,50 +246,52 @@
     	margin-bottom: 30rpx;
     	line-height: 62rpx;
     }
-    .img-box image{
+    .img-box .img{
     	width: 40rpx;
     	height: 40rpx;
     	vertical-align: middle;
+    	margin:16rpx auto;
     }
     .sweep-box{
     	background: #538EEB;
     	width: 170rpx;
     	height: 170rpx;
     	border-radius: 100%;
-    	position: fixed;
+    	position: absolute;
     	left:50%;
     	margin-left: -90rpx;
-    	bottom:90rpx;
+    	bottom:0;
     	text-align: center;
     	box-sizing: border-box;
     	padding-top: 35rpx;
     }
-    .sweep-box image{
+    .sweep-box .img{
 		width: 40rpx;
     	height: 40rpx;
     	display: block;
     	margin: 0 auto;
     }
-    .sweep-box span{
+    .sweep-box .text{
 		color: #fff;
 		margin-top: 25rpx;
 		font-size: 14px;
     }
     .layer{
     	background: rgba(0,0,0,.6);
-    	position: fixed;
+    	position: absolute;
     	width: 100%;
     	height: 100%;
     	left:0;
     	top:0;
+    	z-index:5;
     }
     .edit-pop{
     	width: 100%;
-    	position: absolute;
+    	position: relative;
     	box-sizing: border-box;
     	left:0;
     	bottom:0;
-    	z-index:2;
+    	z-index:15;
     	background: #efefef;
     }
     .close-btn{
