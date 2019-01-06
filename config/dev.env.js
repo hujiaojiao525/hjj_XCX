ceacfecec0170086e473caac57d31f4a10034301
hujiaojiao <hujiaojiao@163.com>
2019-01-06 16:20:10 +0800

    删除多余的代码

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,11 +4,5 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
     BASE_URL: '"https://cdz.0762ld.com"',
-    MAPI_URL: '"https://mapi.test.cheyipai.com"',
-    Monitor:'"https://monitor.test.cheyipai.com/"',
-    SOUCHE_URL: '"https://wechat.proxy.dasouche.com"',
-    IMG_PATH:'"https://i.test.268v.com/"',
-    DSC_DIC_URL: '"https://bulbous.proxy.dasouche.net"',
-    SOU_CHE_REQ_URL: '"https://athena.souche.com"',
 
 })
--- config/pre.env.js
+++ config/pre.env.js
@@ -1,10 +1,4 @@
 module.exports = {
     NODE_ENV: '"pre"',
-    BASE_URL: '"https://uniapi.cheyipai.com"',
-    MAPI_URL: '"https://mapi.cheyipai.com"',
-    Monitor:'"https://monitor.cheyipai.com/"',
-    SOUCHE_URL: '"https://wechat.souche.com"',
-    IMG_PATH:'"https://i.268v.com/"',
-    DSC_DIC_URL: '"https://bulbous.souche.com"',
-    SOU_CHE_REQ_URL: '"https://athena.souche.com"',
+    BASE_URL: '"https://cdz.0762ld.com"',
 }
